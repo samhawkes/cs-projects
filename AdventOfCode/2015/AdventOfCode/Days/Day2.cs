357c4f6fe26cd407ded2354ad65b11e4285756eb
samhawkes <40278032+samhawkes@users.noreply.github.com>
2018-09-17 17:14:21 +0100

    Day 2 part 2
    Completed day 2 part 2. It might be a little convoluted, and there is a little code reuse, but it works!

--- AdventOfCode/2015/AdventOfCode/Days/Day2.cs
+++ AdventOfCode/2015/AdventOfCode/Days/Day2.cs
@@ -23,39 +23,85 @@ namespace AdventOfCode.Days
             reader.Close();
 
             int totalPaper = 0;
+            int totalRibbon = 0;
 
             foreach (var box in list)
             {
-                totalPaper += CalculateSquareFeetOfPaper(box);
+                var boxSides = CalculateBoxSides(box);
+
+                totalPaper += CalculateSquareFeetOfPaper(boxSides);
+                totalRibbon += CalculateRibbonNeeded(box, boxSides);
             }
 
             Console.WriteLine($"The total paper required is: {totalPaper} square feet.");
+            Console.WriteLine($"The total ribbon required is: {totalRibbon} feet.");
         }
-	
-        private int CalculateSquareFeetOfPaper(string dimensions)
+
+        private int CalculateRibbonNeeded(string dimensions, Dictionary<string, int> boxSides)
         {
             var numbers = dimensions.Split('x');
 
             int length = int.Parse(numbers[0]);
             int width = int.Parse(numbers[1]);
             int height = int.Parse(numbers[2]);
-            int boxArea = 0;
 
-            List<int> boxSides = new List<int>
+            int boxVolume = length * width * height;
+            int bow = boxVolume;
+
+            var smallestSide = boxSides.Where(x => x.Value.Equals(boxSides.Min(kvp => kvp.Value)));
+
+            int perimeter = 0;
+
+            switch (smallestSide.First().Key)
             {
-                length * width,
-                width * height,
-                height * length
-            };
+                case ("lw"):
+                    perimeter = (2 * length) + (2 * width);
+                    break;
 
-            int smallestSide = boxSides.Min();
+                case ("wh"):
+                    perimeter = (2 * width) + (2 * height);
+                    break;
+
+                case ("hl"):
+                    perimeter = (2 * height) + (2 * length);
+                    break;
+
+                default:
+                    break;
+            }
+
+            return perimeter + bow;
+        }
+
+        private int CalculateSquareFeetOfPaper(Dictionary<string, int> boxSides)
+        {
+            var boxArea = 0;
+            var smallestSide = boxSides.Min(kvp => kvp.Value);
 
             foreach (var side in boxSides)
             {
-                boxArea += side * 2;
+                boxArea += side.Value * 2;
             }
 
             return boxArea + smallestSide;
+        }
+
+        private Dictionary<string, int> CalculateBoxSides(string dimensions)
+        {
+            var numbers = dimensions.Split('x');
+
+            int length = int.Parse(numbers[0]);
+            int width = int.Parse(numbers[1]);
+            int height = int.Parse(numbers[2]);
+
+            Dictionary<string, int> boxSides = new Dictionary<string, int>
+            {
+                { "lw", length * width },
+                { "wh", width * height },
+                { "hl", height * length }
+            };
+
+            return boxSides;
         }
 
     }
