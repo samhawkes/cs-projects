7f7588d8e8ba32577a106e61542c0af43dd9fdce
samhawkes <40278032+samhawkes@users.noreply.github.com>
2018-09-20 16:09:18 +0100

    Day 6 part 2 completed
    Completed part 2, and refactored out the logic in to some methods.

--- AdventOfCode/2015/AdventOfCode/Days/Day6.cs
+++ AdventOfCode/2015/AdventOfCode/Days/Day6.cs
@@ -35,55 +35,88 @@ namespace AdventOfCode.Days
 
             foreach (var instruction in list)
             {
-                List<string> coords = Regex.Matches(instruction, @"[0-9]{1,3}").Cast<Match>().Select(m => m.Value).ToList();
-
-                int xStart = int.Parse(coords[0]);
-                int yStart = int.Parse(coords[1]);
-                int xEnd = int.Parse(coords[2]);
-                int yEnd = int.Parse(coords[3]);
-
-                var applicableLights = grid.Where(l => xStart <= l.X && l.X <= xEnd && yStart <= l.Y && l.Y <= yEnd);
-
-                if (instruction.Contains("turn on"))
-                {
-                    foreach (var light in applicableLights)
-                    {
-                        light.Status = true;
-                    }
-                }
-                else if (instruction.Contains("turn off"))
-                {
-                    foreach (var light in applicableLights)
-                    {
-                        light.Status = false;
-                    }
-                }
-                else if (instruction.Contains("toggle"))
-                {
-                    foreach (var light in applicableLights)
-                    {
-                        light.Status = !light.Status;
-                    }
-                }
+                grid = this.CalculateLightStatus(grid, instruction);
             }
 
-            var answer = grid.Where(l => l.Status == true).Count();
+            var lightsOn = grid.Where(l => l.Status == true).Count();
+            var brightness = this.CalculateTotalBrightness(grid);
 
-            Console.WriteLine($"The number of lights still on at the end is: {answer}");
+            Console.WriteLine($"The number of lights still on at the end is: {lightsOn}."); //Part 1
+            Console.WriteLine($"The total brightness at the end is: {brightness}."); //Part 2
         }
 
-        internal class Light
+        private List<Light> CalculateLightStatus(List<Light> grid, string instruction)
         {
-            internal Light(int x, int y)
+            List<string> coords = Regex.Matches(instruction, @"[0-9]{1,3}").Cast<Match>().Select(m => m.Value).ToList();
+
+            int xStart = int.Parse(coords[0]);
+            int yStart = int.Parse(coords[1]);
+            int xEnd = int.Parse(coords[2]);
+            int yEnd = int.Parse(coords[3]);
+
+            var applicableLights = grid.Where(l => xStart <= l.X && l.X <= xEnd && yStart <= l.Y && l.Y <= yEnd);
+
+            if (instruction.Contains("turn on"))
             {
-                this.X = x;
-                this.Y = y;
-                this.Status = false;
+                foreach (var light in applicableLights)
+                {
+                    light.Status = true;
+                    light.Brightness++;
+                }
+            }
+            else if (instruction.Contains("turn off"))
+            {
+                foreach (var light in applicableLights)
+                {
+                    light.Status = false;
+                    light.DecrementBrightness();
+                }
+            }
+            else if (instruction.Contains("toggle"))
+            {
+                foreach (var light in applicableLights)
+                {
+                    light.Status = !light.Status;
+                    light.Brightness += 2;
+                }
             }
 
-            internal int X { get; }
-            internal int Y { get; }
-            internal bool Status { get; set; }
+            return grid;
+        }
+
+        private int CalculateTotalBrightness(List<Light> grid)
+        {
+            var totalBrightness = 0;
+
+            foreach (var light in grid)
+            {
+                totalBrightness += light.Brightness;
+            }
+
+            return totalBrightness;
+        }
+    }
+
+    internal class Light
+    {
+        internal Light(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Status = false;
+        }
+
+        internal int X { get; }
+        internal int Y { get; }
+        internal bool Status { get; set; }
+        internal int Brightness { get; set; }
+
+        internal void DecrementBrightness()
+        {
+            if (this.Brightness > 0)
+                this.Brightness -= 1;
+            else
+                this.Brightness = 0;
         }
     }
 }
