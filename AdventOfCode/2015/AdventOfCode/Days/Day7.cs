c81cb563c38d974f71e576492f232f568370c0ae
samhawkes <40278032+samhawkes@users.noreply.github.com>
2018-09-21 11:46:30 +0100

    Day7 starting files
    Input and basic class file for day 7

---
+++ AdventOfCode/2015/AdventOfCode/Days/Day7.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Days
+{
+    public class Day7 : IPuzzleDay
+    {
+        public void Run(string path)
+        {
+            var reader = new StreamReader(path);
+            var list = new List<string>();
+
+            do
+            {
+                var line = reader.ReadLine();
+                list.Add(line);
+            }
+            while (!reader.EndOfStream);
+            reader.Close();
+        }
+    }
+}
