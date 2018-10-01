6cf5b90802553103bdf6d0165357fc181cf05648
samhawkes <40278032+samhawkes@users.noreply.github.com>
2018-10-01 12:33:01 +0100

    More brain dump
    Removed the logic enum, and started trying to implement classes for instructions and components. It's a bit of a brain-dump work-in-progress until I get it working.

--- AdventOfCode/2015/AdventOfCode/Days/Day7.cs
+++ AdventOfCode/2015/AdventOfCode/Days/Day7.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AdventOfCode.Days
@@ -18,37 +19,89 @@ namespace AdventOfCode.Days
              * When trying to evaluate the left, look up what creates it from the right and work backwards 
              */
 
-            //this regex matches each part of the input string in a separate category
-            //"(?-i)(?<leftValue>[a-z0-9]*)?? ?(?<logicOperator>RSHIFT|LSHIFT|OR|NOT|AND)?? ?(?<rightValue>[a-z0-9]*)?? ?-> (?<answerValue>[a-z0-9]*)"
+            var formattedInstructions = this.FormatInstructions(list);
 
-            foreach (var instruction in list)
-            {
-                UInt16 x = 0;
-                UInt16 y = 0;
-                UInt16 z = 0;
+            //var noLogicInstructions = formattedInstructions.Where(x => x.LogicOperator.Equals(string.Empty));
 
-                if (instruction.Contains(LogicActions.AND.ToString()))
-                    z = x & y;
-                else if (instruction.Contains(LogicActions.NOT.ToString()))
-                    z = ~x;
-                else if (instruction.Contains(LogicActions.OR.ToString()))
-                    z = x | y;
-                else if (instruction.Contains(LogicActions.LSHIFT.ToString()))
-                    z = x << y;
-                else if (instruction.Contains(LogicActions.RSHIFT.ToString()))
-                    z = x >> y;
-            }
+            //UInt16 x = 0;
+            //UInt16 y = 0;
+            //UInt16 z = 0;
 
+            //if (instruction.Equals("AND"))
+            //    z = x & y;
+            //else if (instruction.Equals("NOT"))
+            //    z = ~x;
+            //else if (instruction.Equals("OR"))
+            //    z = x | y;
+            //else if (instruction.Equals("LSHIFT"))
+            //    z = x << y;
+            //else if (instruction.Equals("RSHIFT"))
+            //    z = x >> y;
 
         }
 
-        private enum LogicActions
+        internal List<Instruction> FormatInstructions(List<string> inputList)
         {
-            AND,
-            NOT,
-            OR,
-            LSHIFT,
-            RSHIFT
+            List<Instruction> instructions = new List<Instruction>();
+
+            foreach (var line in inputList)
+            {
+                var rgx = @"(?-i)(?<leftValue>[a-z0-9]*)?? ?(?<logicOperator>RSHIFT|LSHIFT|OR|NOT|AND)?? ?(?<rightValue>[a-z0-9]*)?? ?-> (?<answerValue>[a-z0-9]*)";
+
+                var matches = Regex.Match(line, rgx).Groups;
+
+                CircuitComponent left = new CircuitComponent(matches["leftValue"].Value);
+                string logic = matches["logicOperator"].Value;
+                CircuitComponent right = new CircuitComponent(matches["rightValue"].Value);
+                CircuitComponent answer = new CircuitComponent(matches["answerValue"].Value);
+
+
+                Instruction instruction = new Instruction(line, left, logic, right, answer);
+
+                instructions.Add(instruction);
+            }
+
+            return instructions;
+        }
+
+        internal class Instruction
+        {
+            internal Instruction(string fullInput, CircuitComponent left, string logicOp, CircuitComponent right, CircuitComponent answer)
+            {
+                this.FullInput = fullInput;
+
+                this.LeftValue = left;
+                this.LogicOperator = logicOp;
+                this.RightValue = right;
+                this.AnswerValue = answer;
+            }
+
+            internal string FullInput { get; }
+
+            internal CircuitComponent LeftValue { get; }
+
+            internal string LogicOperator { get; }
+
+            internal CircuitComponent RightValue { get; }
+
+            internal CircuitComponent AnswerValue { get; set; }
+        }
+
+        internal class CircuitComponent
+        {
+            internal CircuitComponent(string name)
+            {
+                this.Name = name;
+
+                if (UInt16.TryParse(name, out UInt16 result))
+                {
+                    this.Value = result;
+                }
+            }
+
+            internal string Name { get; }
+
+            internal UInt16? Value { get; set; }
         }
     }
 }
